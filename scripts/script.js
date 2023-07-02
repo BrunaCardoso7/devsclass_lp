bb1e377d96122764798c604c9d944ba887d1cd89
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-07-01 23:26:31 -0300

    update services part

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,25 @@
         conteiner_link_navbar.classList.toggle('active')
            
     }
+
+    const btn_more = [...document.querySelectorAll('.btn_more')]
+    btn_more.forEach(btnMore=>{
+        btnMore.addEventListener('click', showMoreText)
+
+    })
+
+
+    function showMoreText(){
+        const tecnology_used = [...document.querySelectorAll('.tecnology_used')]
+        console.log(tecnology_used)
+        tecnology_used.forEach(moreInfo=>{
+            console.log(moreInfo.target)
+            if(moreInfo.style.display == 'none'){
+                moreInfo.style.display = 'block'
+            }else{
+                moreInfo.style.display = 'none'
+            }    
+        })
+    }
+
 })()
