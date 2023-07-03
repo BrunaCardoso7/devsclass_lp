ab2b8c79f3d08901ff1c60d29b94ec66e3e223b1
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-07-03 15:56:47 -0300

    finish services part

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@
            
     }
 
+
     const btn_more = [...document.querySelectorAll('.btn_more')]
     btn_more.forEach(btnMore=>{
         btnMore.addEventListener('click', showMoreText)
@@ -16,17 +17,18 @@
     })
 
 
-    function showMoreText(){
-        const tecnology_used = [...document.querySelectorAll('.tecnology_used')]
-        console.log(tecnology_used)
-        tecnology_used.forEach(moreInfo=>{
-            console.log(moreInfo.target)
-            if(moreInfo.style.display == 'none'){
-                moreInfo.style.display = 'block'
-            }else{
-                moreInfo.style.display = 'none'
-            }    
-        })
-    }
+    function showMoreText(event){
+        const btn_info = event.target
+        const moreInfo = event.target.nextElementSibling
+        console.log(moreInfo)
+        if(moreInfo.style.display == 'none'){
+            moreInfo.style.display = 'block'
+            btn_info.textContent = 'Mostrar menos'
 
+        }else{
+            moreInfo.style.display = 'none'
+            btn_info.textContent = 'Mostrar mais'
+        }    
+
+    }
 })()
