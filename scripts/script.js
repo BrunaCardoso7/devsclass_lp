7b36219f4e14846947ed383c2dc2dc065aa5f43a
BrunaCardoso7 <126504526+BrunaCardoso7@users.noreply.github.com>
2023-07-11 19:47:29 -0300

    refaturação

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 ;(function () {
+
+  
+
     const menu_burger = document.querySelector('#menu_burger')
     menu_burger.addEventListener('click', showMenu)
-
-
     function showMenu(){
         const conteiner_link_navbar = document.querySelector('.conteiner_link_navbar')
         conteiner_link_navbar.classList.toggle('active')
-           
     }
+
+
 
 
     const btn_more = [...document.querySelectorAll('.btn_more')]
@@ -15,8 +17,6 @@
         btnMore.addEventListener('click', showMoreText)
 
     })
-
-
     function showMoreText(event){
         const btn_info = event.target
         const moreInfo = event.target.nextElementSibling
@@ -24,11 +24,9 @@
         if(moreInfo.style.display == 'none'){
             moreInfo.style.display = 'block'
             btn_info.textContent = 'Mostrar menos'
-
         }else{
             moreInfo.style.display = 'none'
             btn_info.textContent = 'Mostrar mais'
         }    
-
     }
 })()
