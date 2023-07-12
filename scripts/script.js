ff275a95a30e6cea6ac63f4840a8b5e2fa7dea5b
BrunaCardoso7 <126504526+BrunaCardoso7@users.noreply.github.com>
2023-07-12 09:35:24 -0300

    concertei alguns bugs na estilização e a responsividade da hero e navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,60 @@
 ;(function () {
+    window.addEventListener('scroll', changeNagation)
+    const conteiner_link_navbar = document.querySelector('.conteiner_link_navbar')
+    let links = conteiner_link_navbar.querySelectorAll('li a')
 
-  
+    function changeNagation(){
+        let position = [...links].map(link=>{
+            let href = link.getAttribute('href')
+            let h2 = document.querySelector(href)
+
+            return h2.getBoundingClientRect().top
+        })
+
+        let activeLink = getLastLinkview(position)
+        let menuActive = conteiner_link_navbar.querySelector('.active')
+    
+        if(menuActive){
+            menuActive.classList.remove('active')
+        } 
+        if(activeLink){
+            activeLink.classList.add('active')
+        }else{
+            links[0].classList.add('active')
+        }
+    }
+    function getLastLinkview(_positions){
+        let positionElement_zero = _positions.filter(elements => elements < innerHeight/2)
+
+        return links[positionElement_zero.length - 1]   
+    }
+    changeNagation()
+
+
 
     const menu_burger = document.querySelector('#menu_burger')
     menu_burger.addEventListener('click', showMenu)
     function showMenu(){
         const conteiner_link_navbar = document.querySelector('.conteiner_link_navbar')
-        conteiner_link_navbar.classList.toggle('active')
+        if(conteiner_link_navbar.style.display == 'none'){
+            conteiner_link_navbar.style.display = 'block'
+        }else{
+            conteiner_link_navbar.style.display = 'none'
+        }
+    }
+    document.addEventListener('DOMContentLoaded', function(){
+        verificationMediaScreen()
+    })
+    window.addEventListener('resize', verificationMediaScreen)
+    function verificationMediaScreen(){
+        if(window.innerWidth <= 800){
+            menu_burger.style.display = 'block'
+        }else{
+            menu_burger.style.display = 'none'
+        }
     }
 
-
+   
 
 
     const btn_more = [...document.querySelectorAll('.btn_more')]
